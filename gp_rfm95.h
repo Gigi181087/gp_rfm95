ba8dadef90eb09463f9ce29b1856d22a20bca68d
Gian Piero Cecchetti <92372758+Gigi181087@users.noreply.github.com>
2023-11-26 23:27:30 +0100

    Update gp_rfm95.h

--- gp_rfm95.h
+++ gp_rfm95.h
@@ -1,13 +1,14 @@
 #ifndef __GP_RFM95_H_
-#define __GP_RFM95_H_
+#define __GP_RFM95_H_#
+
+#include <stdint.h>
 
 //errorcodes
-
 #define GP_RFM95_ERRORS_NOERROR      0
 
 typedef struct gp_rfm95 gp_rfm95_t;
 
-void gp_rfm95_init(gp_rfm95_t**);
+uint8_t gp_rfm95_init(gp_rfm95_t**);
 
 
 
